57e51e879765e4477e577f4f9f06db78578e898d
heymen <ay.alloune@gmail.com>
2024-02-02 13:03:34 +0100

    modif about

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,7 @@
 export const aboutData = {
     title: "Qui suis-je ?",
-    description1: "Je m'appel Aymen. Je suis un développeur qui en est à sa troisième année de BUT-informatique, situé à l'université Sorbonne Paris Nord.",
-    description2:  "Principalement j'étudie à l'université en suivant les cours et en travaillant sur les projets universitaires qui nous sont donnés. Je prends du temps pour m'exercer en faisant du sport. Sur mon temps libre je travaille sur des projets personnels et j'essaye de m'informer sur les nouvelles technologies comme l'I.A.",
+    description1: "Étudiant en troisième année de BUT informatique à l'université Sorbonne Paris Nord passionné par le développement d'application et l'informatique. Coder, confectionner des algorithmes ou les résoudre est ce qui m'intéresse le plus dans l'informatique et c'est ce que je continue de faire à l'intérieur et à l'extérieur de ma formation.\n",
+    description2:  "En ce moment je suis à la recherche d'un stage de 4 mois à partir du 11 mars pour terminer ma troisième année, si mon profil vous intéresse n'hésitez surtout pas à me contacter. Pour toutes question n'hésitez pas à me contacter par téléphone ou par mail pour une réponse rapide !\n" +
+        "Merci pour votre temps.",
     image: 1
 }
